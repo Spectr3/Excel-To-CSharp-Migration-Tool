c069f5f0146b49ac514225c28e016d3fece62652
Martyn Price <martyn@devoria.co.uk>
2023-06-27 11:26:44 +0100

    V1

---
+++ ExcelToModelGenerator/ExcelToModelGenerator/ClassGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToModelGenerator
+{
+    public class ClassGenerator
+    {
+        public string GenerateClass(string namespaceName, string className, dynamic[] properties, bool addDecorators)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine($"namespace {namespaceName}");
+            builder.AppendLine("{");
+
+            if (addDecorators)
+            {
+                builder.AppendLine($"\t[WorkbookName(\"{className}\")]");
+            }
+
+            builder.AppendLine($"\tpublic class {className}");
+            builder.AppendLine("\t{");
+
+            foreach (var property in properties)
+            {
+                if (addDecorators)
+                {
+                    builder.AppendLine($"\t\t[WorkbookItemName(\"{property.OriginalName}\")]");
+                }
+
+                builder.AppendLine($"\t\tpublic string {property.CleanName} {{ get; set; }} = string.Empty;");
+            }
+
+            builder.AppendLine("\t}");
+            builder.AppendLine("}");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ ExcelToModelGenerator/ExcelToModelGenerator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelToModelGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "PATH TO EXCEL SHEET";
+
+            Console.WriteLine("Please enter the namespace for the generated classes:");
+            var namespaceName = Console.ReadLine();
+
+            Console.WriteLine("Do you want to add decorators to the classes and properties? (yes/no):");
+            var addDecorators = Console.ReadLine()?.ToLower() == "yes";
+
+            Console.WriteLine("Please enter the name of the folder to place the class files in:");
+            var folderName = Console.ReadLine();
+
+            if (!Directory.Exists(folderName))
+            {
+                Directory.CreateDirectory(folderName);
+            }
+
+            var generator = new ClassGenerator();
+
+            var app = new Application();
+            var workbook = app.Workbooks.Open(filePath);
+
+            foreach (Worksheet worksheet in workbook.Worksheets)
+            {
+                var originalClassName = worksheet.Name;
+                var className = CleanUpClassName(originalClassName);
+                var headers = worksheet.Range["1:1"].Cells.Cast<Microsoft.Office.Interop.Excel.Range>()
+                    .TakeWhile(cell => cell.Value2 != null)
+                    .Select(cell => new { OriginalName = cell.Value2.ToString(), CleanName = CleanUpPropertyName(cell.Value2.ToString()) })
+                    .ToArray();
+
+                var classDefinition = generator.GenerateClass(namespaceName, className, headers, addDecorators);
+                File.WriteAllText(Path.Combine(folderName, $"{className}.cs"), classDefinition);
+            }
+
+            workbook.Close();
+            app.Quit();
+        }
+
+        private static string CleanUpPropertyName(string propertyName)
+        {
+            var cleanName = propertyName.Replace(" ", "_")
+                .Replace("-", "_")
+                .Replace("/", "_");
+
+            if (char.IsDigit(cleanName[0]))
+            {
+                cleanName = "_" + cleanName;
+            }
+
+            return cleanName;
+        }
+
+        private static string CleanUpClassName(string className)
+        {
+            var cleanName = className.Replace(" ", "_")
+                .Replace("-", "_")
+                .Replace("/", "_");
+
+            if (char.IsDigit(cleanName[0]))
+            {
+                cleanName = "_" + cleanName;
+            }
+
+            return cleanName;
+        }
+    }
+}
